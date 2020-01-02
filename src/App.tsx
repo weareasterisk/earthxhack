075c10cd690d0bcb16f98a52fe56735b5810f172
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-02 02:19:59 -0600

    feat: init react router with metadata in helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
+import { Helmet } from "react-helmet";
+import HomePage from "./components/pages/homepage" 
+
+const eventLocation = {
+  "location": {
+    "@type": "Place",
+    "name": "Embarcadero Building, Fair Park",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "3809 Grand Avenue",
+      "addressLocality": "Dallas",
+      "postalCode": "75223",
+      "addressRegion": "TX",
+      "addressCountry": "US"
+    }
+  },
+};
+
+const eventData = {
+  "@context": "https://schema.org",
+  "@type": "Event",
+  "name": "EarthxHack",
+  "startDate": "2020-04-24",
+  "endDate": "2020-04-26",
+  eventLocation,
+  "image": [
+    "https://cdn.weareasterisk.com/hackathon-assets/hackdfw/logos/png/hackdfw-1x1-color.png",
+    "http://cdn.weareasterisk.com/hackathon-assets/hackdfw/artwork/banner-hd-large.png"
+  ],
+  "description": "EarthxHack is a hackathon designed to inspire you to use your creativity, technical thinking, and knowledge to solve real issues plaguing the world today. We want to provide a space to help inspire creative solutions, so you can create inspirational ideas that can be implemented on a global scale. We encourage you to come and join us in making new things and turning the world into a better place!",
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8"/>
+        <title>EarthxHack | The Environmental Hackathon of Dallas</title>
+        <script className="structured-data-list" type="application/ld+json">
+          { JSON.stringify(eventData) }
+        </script>
+      </Helmet>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
