0372302d0ca14645d742b87562d4d3b2b496c618
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-02 05:07:10 -0600

    feat: introduce initial phases data

---
+++ src/data/phases.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    title: "Part 0: Learn",
+    period: "April 24th, 12:00pm-6:00pm",
+    tagline: "A series of hands-on interactive workshops, talks, and experiences, both in person and live streamed.",
+    content: [
+      "Learn is a series of workshops and lightning talks about subjects that are relevant to the world and to this event. You can expect a variety of sessions around technical topics, environmental issues, inspiration for your projects, ideation and coming up with cool thoughts, how to build your projects, and more!",
+      "Talks and sessions will be delivered in person at Fair Park and will also be live streamed. You are welcome to join us for any portion of the session. Applications are not required for this portion of the event."
+    ],
+    image: "//cdn.weareasterisk.com/"
+  },
+  {
+    title: "Part 1: Dream",
+    period: "April 24th, 6:00pm-10:00am",
+    tagline: "An ideation and prototyping competition with no technology.",
+    content: [
+      "Dream is a mini-hackathon designed to allow you to focus solely on an idea and its non-technical implementation. Created for those who have limited programming or technical experience and want to participate in helping to make the world a better place through innovative solutions to environmental problems, Dream is the place for you. We’ll offer a complete mini-hackathon experience, where you can join others and come up with new ideas - then design small non-technical prototypes and present them to our judging panel to win prizes. No experience or prior knowledge is required - just your drive to make cool stuff happen.",
+      "During Dream, we’ll offer workshops, talks, fun activities, meals and snacks, and that all-night ideation frenzy that hackathons are famous for.",
+      "Like your ideas a lot and want to actually make them happen through technology? Carry over your idea into Build, team up with new or the same people, and become eligible for additional prizes in Build."
+    ],
+    image: "//cdn.weareasterisk.com/"
+  },
+  {
+    title: "Part 2: Build",
+    period: "April 25th, 9:00am - April 26th, 4:00pm",
+    tagline: "A traditional hackathon format, with a twist.",
+    content: [
+      "Build is the traditional EarthxHack experience that you’ve come to know and love. We’ve designed a hackathon for you to collaborate and innovate with environmental partners from the festival, as well as your peers and new friends. Come up with interesting technical ideas and implement them to be eligible for prizes.",
+      "This year, we’re offering collaboration prizes. Team up with a booth from the festival to work on your project and you can both win prizes. In addition, you can use ideas from our Dream phase to build your own projects. Work with collaborators from Dream and become eligible for additional prizes."
+    ]
+  }
+]
+
+export default data;
