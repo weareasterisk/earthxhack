6755655e85b96771a26d58b9257bcdc216f6d96c
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-02 21:33:56 -0600

    chore: mount phases to home page

--- src/components/pages/homepage.tsx
+++ src/components/pages/homepage.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import About from "../homepage/about";
-import Phases from "../homepage/phases/phasesaggregrator"
-import PhaseData from "../../data/phases"
+import Phases from "../homepage/phases/phasesAggregrator"
+import PhaseData from "../../config/phases"
 
 export default class HomePage extends Component {
   render() {
