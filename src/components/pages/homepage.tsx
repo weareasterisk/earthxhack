0e732d456c5d3ddb8c63355fc5633145b88ae80b
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-03 05:47:05 -0600

    feat: add image spacer below phases

--- src/components/pages/homepage.tsx
+++ src/components/pages/homepage.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import About from "../homepage/about";
 import Phases from "../homepage/phases/phasesAggregrator"
 import PhaseData from "../../config/phases"
+import ImageSpacer from "../common/imageSpacer"
 
 export default class HomePage extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class HomePage extends Component {
       <React.Fragment>
         <About/>
         <Phases data={PhaseData}/>
+        <ImageSpacer image="https://cdn.weareasterisk.com/event-assets/earthxhack/artwork/mountains-min.png"/>
       </React.Fragment>
 
     )
