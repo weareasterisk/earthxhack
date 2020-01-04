9eefe7b2ad320f8ce6877352d298f603289ded21
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-03 18:22:31 -0600

    feat: add component for underlined titles

---
+++ src/components/common/underlinedTitle.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+interface IUnderlinedTitle {
+  color: string,
+  className?: string,
+  style?: React.CSSProperties,
+  children?:  JSX.Element | Array<JSX.Element> | undefined | Array<undefined> | Element | Array<Element> | object | string,
+  width?: string,
+  height?: string,
+  grow?: "left" | "right",
+  lineClasses?: string
+}
+
+const UnderlinedTitle: React.FC<IUnderlinedTitle> = ({color, className, style, children, width = "100%", height="2rem", grow, lineClasses}) => {
+  const commonStyles = {
+    // height: height,
+    backgroundColor: color
+  }
+
+  return (
+    <React.Fragment>
+      <div className={`${className}`} style={{
+        lineHeight: "100%",
+        ...style
+      }}>
+        {children}
+        <div className={`relative`} style={{ zIndex: -500}}>
+          <div style={{
+            // marginTop: `calc(-1 * (${height} / 1.3))`,
+            transform: "translate(0, -80%)",
+            width: width,
+            ...commonStyles
+          }}
+          className={lineClasses}>
+            { grow && grow === "left" &&
+              <div 
+              className={`absolute overflow-hidden w-screen ${lineClasses}`}
+              style={{
+                left: "-100vw",
+                ...commonStyles
+              }}/>
+            }
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default UnderlinedTitle
