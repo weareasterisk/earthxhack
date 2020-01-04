f624114f85e03b6c9590c267670e601938eec657
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-04 05:12:21 -0600

    fix: remove height derivation and correctly set alt

--- src/components/common/decoratorImage.tsx
+++ src/components/common/decoratorImage.tsx
@@ -4,16 +4,14 @@ interface IDecoratorImage {
   src: string,
   alt?: string,
   className?: string,
-  imageClassName?: string,
-  maxWidth: string
-  maxMobileWidth?: string
+  imageClassName?: string
 }
 
-const DecoratorImage: React.FC<IDecoratorImage> = ({src, alt, className, imageClassName, maxWidth, maxMobileWidth}) => {
+const DecoratorImage: React.FC<IDecoratorImage> = ({src, alt, className, imageClassName}) => {
   return (
     <React.Fragment>
       <div className={`relative w-full items-center lg:justify-end flex justify-around ${className}`}>
-        <img className={`inline-block h-auto ${maxMobileWidth ? `w-${maxMobileWidth}` : ""} lg:w-${maxWidth} ${imageClassName}`} src={src} alt={alt? alt : ""}/>
+        <img className={`inline-block h-auto ${imageClassName}`} src={src} alt={alt? alt : undefined}/>
       </div>
     </React.Fragment>
   )
