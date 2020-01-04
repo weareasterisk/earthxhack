0ab76b06a572c17a93e221770b63e8d1e12e1eb8
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-04 05:08:54 -0600

    build(webpack): use postcss to build style

---
+++ postcss.config.js
@@ -0,0 +1,38 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
+const isProd = process.env.NODE_ENV === "production"
+
+module.exports = {
+  modules: true,
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer')({
+      "grid": true
+    }),
+    require('postcss-url'),
+    require('postcss-nested'),
+    require('postcss-preset-env')({
+      autoprefixer: { grid: true },
+      features: {
+          'nesting-rules': true
+      },
+      browsers: [
+          '> 1%',
+          'last 2 versions',
+          'Firefox ESR',
+      ]
+    }),
+
+    isProd 
+    ? cssnano({preset: 'default'}) 
+    : null,
+
+    isProd
+    ? purgecss({
+      content: ['./public/**/*.html', './src/**/*.tsx'],
+      defaultExtractor: content => content.match(/[\w-/:%.]+(?<!:)/g) || []
+    }) : null
+  ],
+};
