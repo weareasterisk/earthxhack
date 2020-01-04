56dff14aa1d79819b572dac7580574fdedecab22
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-03 20:37:58 -0600

    feat: add generic component for a lazily loaded image

---
+++ src/components/common/lazyImage.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+interface ILazyImageProps {
+  src: string,
+  alt?: string,
+  className?: string,
+  style?: React.CSSProperties,
+  placeholder?: string
+}
+
+interface ILazyImageState {
+  src: string | undefined,
+  placeholder: string | undefined
+}
+
+export default class LazyImage extends React.Component<ILazyImageProps, ILazyImageState> {
+  constructor(props: ILazyImageProps) {
+    super(props)
+    this.state = {
+      src: "",
+      placeholder: ""
+    }
+  }
+
+  async componentDidMount() {
+    const { src, placeholder } = this.props
+    const imageLoader = new Image();
+    imageLoader.src = src;
+    imageLoader.onload = () => {
+      this.setState({ src })
+    }
+    if (!!placeholder) {
+      const placeholderLoader = new Image();
+      placeholderLoader.src = placeholder
+      placeholderLoader.onload = () => {
+        this.setState({ placeholder })
+      }
+    }
+  }
+
+  render() {
+    const { src, placeholder } = this.state
+    const { className, style, alt, children } = this.props;
+    return (
+      <React.Fragment>
+        <img className={`${className}`}
+          style={{
+            ...style
+          }}
+          src={!!src ? src : (!!placeholder ? placeholder : "")}
+          alt={alt}
+        />
+      </React.Fragment>
+    )
+  }
+}
