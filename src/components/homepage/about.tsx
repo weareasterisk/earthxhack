dad35b7e22ae1a2478abb55964f1d925fb4198fc
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-04 05:14:22 -0600

    fix: conform to new decoratoriamge standard

--- src/components/homepage/about.tsx
+++ src/components/homepage/about.tsx
@@ -25,7 +25,7 @@ const About: React.FC = () => {
                 difference for the future.
               </p>
             </div>
-            <DecoratorImage className="lg:pb-1 lg:pt-0 pt-1.8125 pb-3" imageClassName="" maxWidth="18.75" maxMobileWidth="10" src="//cdn.weareasterisk.com/event-assets/earthxhack/artwork/poly-earth.svg" alt="poly earth"/>
+            <DecoratorImage className="lg:pb-1 lg:pt-0 pt-1.8125 pb-3" imageClassName="lg:w-18.75 w-10" src="//cdn.weareasterisk.com/event-assets/earthxhack/artwork/poly-earth.svg" alt="poly earth"/>
           </div>
         </div>
       </ContentWrapper>
