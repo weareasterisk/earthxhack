23d3732577e82c0be2939ce3c87b91a1010785a8
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-04 21:38:10 -0600

    chore: use lazyImage component with optional placeholder

--- src/components/common/decoratorImage.tsx
+++ src/components/common/decoratorImage.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import LazyImage from "../common/lazyImage"
 
 interface IDecoratorImage {
   src: string,
+  placeholder?: string,
   alt?: string,
   className?: string,
   imageClassName?: string
 }
 
-const DecoratorImage: React.FC<IDecoratorImage> = ({src, alt, className, imageClassName}) => {
+const DecoratorImage: React.FC<IDecoratorImage> = ({src, placeholder, alt, className, imageClassName}) => {
   return (
     <React.Fragment>
       <div className={`relative w-full items-center lg:justify-end flex justify-around ${className}`}>
-        <img className={`inline-block h-auto ${imageClassName}`} src={src} alt={alt? alt : undefined}/>
+        <LazyImage className={`inline-block h-auto ${imageClassName}`} src={src} alt={alt} placeholder={placeholder}/>
       </div>
     </React.Fragment>
   )
