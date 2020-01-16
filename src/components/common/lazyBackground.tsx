06deb30be47a8589aa79ad686ac344c3b28bfceb
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-16 00:36:02 -0600

    refactor: make lazy background image component more explicit

--- src/components/common/lazyBackground.tsx
+++ src/components/common/lazyBackground.tsx
@@ -1,47 +1,64 @@
 import React from "react";
-import * as ImageUtilities from "../../utilities/imageUtilities"
 
 interface LazyBackgroundProps {
   src: string,
-  placeholder: string,
+  placeholder?: string,
   className?: string,
   style?: React.CSSProperties,
   children: JSX.Element | Array<JSX.Element> | undefined | Array<undefined> | Element | Array<Element> | object,
 }
 
 interface LazyBackgroundState {
-  src: string | null
+  src: string | undefined | null,
+  loaded: boolean
 }
 
 export default class LazyBackground extends React.Component<LazyBackgroundProps, LazyBackgroundState> {
   constructor(props: LazyBackgroundProps) {
     super(props)
     this.state = {
-      src: null
+      src: this.props.placeholder,
+      loaded: false
     }
   }
 
-  componentDidMount() {
+  async loadImage(src: string) {
+    const imageLoader = new Image();
+    imageLoader.onload = () => {
+      this.setState({ src, loaded: true })
+    }
+    imageLoader.src = src;
+  }
+
+  async componentDidMount() {
     const { src } = this.props
 
-    const imageLoader = new Image()
-    imageLoader.src = src;
-
-    imageLoader.onload = () => {
-      this.setState({ src })
-    }
+    Promise.resolve(this.loadImage(src))
   }
 
   render() {
     const { src } = this.state
-    const { className, placeholder, style } = this.props
+    const { className, style } = this.props
+
     return (
-      <React.Fragment>
+      this.state.loaded 
+      ? <React.Fragment>
         <div
           className={`background ${className}`}
           style={{
-            backgroundImage: ImageUtilities.standardizeUrl(src || placeholder),
-            ...{blur: `${(!src ? "blur(8px)" : "")}`},
+            backgroundImage: `url(${this.state.src})`,
+            ...style
+          }}
+        >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+      : <React.Fragment>
+        <div
+          className={`background ${className}`}
+          style={{
+            backgroundImage: `url(${this.props.placeholder})`,
+            ...{blur: `${("blur(8px)")}`},
             ...style
           }}
         >
