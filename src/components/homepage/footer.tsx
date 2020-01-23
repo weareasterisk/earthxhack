28f276e3bf936e0e7544ccff543fc775df91fe32
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-23 03:07:26 -0600

    fix: auto determine year on copyright

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
     <footer>
       <ContentWrapper className="md:pb-3 pb-1.5 md:text-lg text-sm">
         <p className="pull-left">
-          © 2019 Asterisk. Made with <FontAwesomeIcon color="red" icon={faHeart}/> in Dallas.
+          © {new Date().getFullYear()} Asterisk. Made with <FontAwesomeIcon color="red" icon={faHeart}/> in Dallas.
           <div className="inline font-semibold">
             {" "}<a className="footer-link" href="https://weareasterisk.com/terms">Terms</a>{" "}|
             {" "}<a className="footer-link" href="https://weareasterisk.com/privacy">Privacy</a>{" "}|
