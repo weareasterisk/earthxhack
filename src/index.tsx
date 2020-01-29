0e915990b6d6bae3a3f8651557bc55ddf85f83b9
Muhammad Khatri <muhammad@weareasterisk.com>
2020-01-29 13:37:16 -0600

    fix: remove react-snap due to compat issues

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,7 @@ import './generated.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const root = document.getElementById('root');
-
-root && root.hasChildNodes() ?
-  ReactDOM.hydrate(<App />, root) :
-  ReactDOM.render(<App/>, root);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
